26c67ffcdb2e7006c4eddd7a15bf0febfd593fca
yacs-1 <chromebook1@youngsvilleacademy.org>
2025-05-27 15:10:29 -0400

    Update time-keeper.js

--- wd-sub/time-keeper.js
+++ wd-sub/time-keeper.js
@@ -1,4 +1,4 @@
- js:let initDate;
+let initDate;
 let hasInitDate=false;
 let timeDifference=[0];
 
@@ -7,7 +7,7 @@ cookieArr=document.cookie.split(";");
 for(const element of cookieArr){
   if(element.split("=")[0]=="init-date"){
     hasInitDate=true;
-    initDate=element.split("=")[1];
+    initDate= new Date(element.split("=")[1]);
   }
 }
 
@@ -16,12 +16,12 @@ if(!hasInitDate){
 }else{
   let now= new Date();
   document.cookie="new-date=" + now + "; max-age=31536000000";
-  timeDifference= now.getTime()-initDate.getTime();
+  timeDiff= now.getTime()-initDate.getTime();
 }
 
 let timeSpent= document.getElementById("time-spent");
 let numHours=Math.floor(timeDifference/360000)
 let numMins=Math.floor((timeDifference % 360000)*60);
 let diffStr= numHours + ":" + numMins;
-timeSpent.innerText=diffStr;
 
+timeSpent.innerText=diffStr;
